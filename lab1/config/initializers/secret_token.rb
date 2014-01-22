7bdf9a029c9ac3a78c8ea5673fda07dbcde2096c
Ryan Bird <rlbird22@gmail.com>
2014-01-22 14:42:49 -0700

    Set up rails application sudo rails new lab1

---
+++ lab1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lab1::Application.config.secret_key_base = 'a9696f6fe80c793c665382cf195854fb6282ed8182bac9d341c7b2322bc43428d3750ef7cdc19b32e3c5d8ac767a60a15543bee9641fb17a50a9182a71f47d82'
