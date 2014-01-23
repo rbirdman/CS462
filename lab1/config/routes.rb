efcf158774378b94496db73f4f43b6c672a0a1b0
Ryan Bird <rlbird22@gmail.com>
2014-01-22 21:42:18 -0700

    Add homepage html to rails server

--- lab1/config/routes.rb
+++ lab1/config/routes.rb
@@ -4,6 +4,7 @@ Lab1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
