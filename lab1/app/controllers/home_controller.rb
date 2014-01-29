bbcabf68a0fbf0e42f484b6d2393643fa4ee5f02
Ryan Bird <rlbird22@gmail.com>
2014-01-29 16:01:33 -0700

    Add ssl capabilities to Rails Application

--- lab1/app/controllers/home_controller.rb
+++ lab1/app/controllers/home_controller.rb
@@ -1,2 +1,34 @@
 class HomeController < ApplicationController
+	include SslRequirement
+	
+	#ssl_required :signup, :payment
+    #ssl_allowed :index
+    #https://github.com/rails/ssl_requirement
+	
+	ssl_required :home
+	ssl_allowed :index
+	
+	def home
+		
+	end
+	
+	def index
+		puts "<html>Received SSL connection</html>"
+	end
+	
+    def signup
+		# Non-SSL access will be redirected to SSL
+    end
+    
+    def payment
+		# Non-SSL access will be redirected to SSL
+    end
+	
+    def index
+		# This action will work either with or without SSL
+    end
+	
+    def other
+		# SSL access will be redirected to non-SSL
+    end
 end
